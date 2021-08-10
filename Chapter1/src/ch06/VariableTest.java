23c6ef61bd83c7aaee7151a6d0244843d45cd591
TDCIAN <tdcian71@gmail.com>
2021-08-10 09:55:44 +0900

    second

--- Chapter1/src/ch06/VariableTest.java
+++ Chapter1/src/ch06/VariableTest.java
@@ -1,5 +1,13 @@
 package ch06;
 
 public class VariableTest {
+	public static void main(String[] args) {
+		byte bnum = -127;
+		
+		System.out.println(bnum);
+		
+		
+		long lNum = 123456789;
+	}
 
 }
